ac67f51b9593e2797d466aeac44f23c8b02b6341
Harald Kirschner <hkirschner@mozilla.com>
2013-09-03 17:35:32 -0700

    Applied fix using mozAudioChannelType, success!

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-'use strict';
+(function() {
+	'use strict';
 
-var audios = document.getElementsByTagName('audio');
+	var audios = document.getElementsByTagName('audio');
 
-// BUG: Does not fire when screen is switched off while audio is playing!
-document.addEventListener('mozvisibilitychange', function() {
-	if (document.mozHidden) {
+	// https://bugzilla.mozilla.org/show_bug.cgi?id=910340#c12
+	Array.forEach(audios, function(audio) {
+		var before = audio.mozAudioChannelType || '""';
+		audio.mozAudioChannelType = 'content';
+		alert(before + ' -> ' + audio.mozAudioChannelType);
+	});
+
+	function onHide() {
 		var paused = Array.filter(audios, function(audio) {
 			if (audio.paused) {
 				return false;
@@ -14,7 +20,9 @@ document.addEventListener('mozvisibilitychange', function() {
 			return true;
 		}).length;
 		log('Change: Hidden. ' + paused + ' playing');
-	} else {
+	}
+
+	function onShow() {
 		var resumed = Array.filter(audios, function(audio) {
 			if (!audio.$forcePaused) {
 				return false;
@@ -25,19 +33,42 @@ document.addEventListener('mozvisibilitychange', function() {
 		}).length;
 		log('Change: Visible. ' + resumed + ' resumed');
 	}
-})
 
-document.addEventListener('blur', function() {
-	log('Blurred');
-});
+	// BUG: Does not fire when screen is switched off while audio is playing!
+	// FIX: Set .mozAudioChannelType on audio element to 'content'
+	document.addEventListener('mozvisibilitychange', function() {
+		if (document.mozHidden) {
+			onHide();
+		} else {
+			onShow();
+		}
+	});
+
+	// BUG/FIX: Works also only with the aforementioned fix
+	/*
+	window.addEventListener('focus', function() {
+		log('window.onfocus');
+		onShow();
+	});
+	window.addEventListener('focusout', function() {
+		log('window.onfocusout');
+		onHide();
+	});
+	window.addEventListener('blur', function() {
+		log('window.onblur');
+		onHide();
+	});
+	*/
 
 
-// Debugging
-var logs = '';
+	// Debugging
+	var logs = '';
 
-function log(msg) {
-	console.log(msg);
-	var el = document.getElementById('log');
-	logs = msg + '\n' + logs;
-	el.textContent = logs;
-}
+	function log(msg) {
+		console.log(msg);
+		var el = document.getElementById('log');
+		logs = msg + '\n' + logs;
+		el.textContent = logs;
+	}
+
+})();
